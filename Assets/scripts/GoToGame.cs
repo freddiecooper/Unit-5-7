dedfba57b9c6b79821df330a42aa1c49261d8bda
freddiecooper <72386496+freddiecooper@users.noreply.github.com>
2021-04-04 14:32:11 +0100

    yes

--- Assets/scripts/GoToGame.cs
+++ Assets/scripts/GoToGame.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class GoToGame : MonoBehaviour
 {
+    public GameObject menuFirst, optionsFirst, instructionsFirst, quitFirst, difficultyFirst, gameFirst;
+
     public void LoadScene(string game)
     { 
          SceneManager.LoadScene(game);
@@ -15,6 +18,37 @@ public class GoToGame : MonoBehaviour
         Debug.Log ("quit!");
         Application.Quit();
     }
+    
+    public void OptionsNav()
+   {
+      EventSystem.current.SetSelectedGameObject(null);
+      EventSystem.current.SetSelectedGameObject(optionsFirst);
+   }
+   public void MainMenuNav()
+   {
+      EventSystem.current.SetSelectedGameObject(null);
+      EventSystem.current.SetSelectedGameObject(menuFirst);
+   }
+   public void InstructionsNav()
+   {
+      EventSystem.current.SetSelectedGameObject(null);
+      EventSystem.current.SetSelectedGameObject(instructionsFirst);
+   }
+   public void QuitNav()
+   {
+      EventSystem.current.SetSelectedGameObject(null);
+      EventSystem.current.SetSelectedGameObject(quitFirst);
+   }
+   public void DifficultyNav()
+   {
+      EventSystem.current.SetSelectedGameObject(null);
+      EventSystem.current.SetSelectedGameObject(difficultyFirst);
+   }
+   public void GameNav()
+   {
+      EventSystem.current.SetSelectedGameObject(null);
+      EventSystem.current.SetSelectedGameObject(gameFirst);
+   }
 
     public void Data()
     {
